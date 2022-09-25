99d132c597be7b9c99cde83c5576349e6089d8a0
Qillie <aorkzlr@qillie.com>
2022-09-25 16:50:07 +0900

    Update CardProductPost

--- builder/src/widgets/CardProductPost/CardProductPost.tsx
+++ builder/src/widgets/CardProductPost/CardProductPost.tsx
@@ -1,3 +1,6 @@
+//* Import libraries
+import { Link } from "react-router-native"
+
 //* Import interfaces
 import ICardProductPost from "./interfaces/ICardProductPost"
 
@@ -10,84 +13,86 @@ import { Box } from "../../core/layout"
 
 const CardProductPost = (props: ICardProductPost) => {
     return (
-        <CardTypePost 
-            borderRadius={6} 
-            src={props.src} 
-            thumbnailWidth={"100%"}
-        >
-            <Box flexDirection="column" pl={14} pb={5}>
-                {/* Title */}
-                <Box>
-                    <Typography
-                        variant="h3"
-                        fontWeight="500"
-                    >
-                        {props.title}
-                    </Typography>
-                </Box>
+        <Link to={props.link}>
+            <CardTypePost 
+                borderRadius={6} 
+                src={props.src} 
+                thumbnailWidth={"100%"}
+            >
+                <Box flexDirection="column" pl={14} pb={5}>
+                    {/* Title */}
+                    <Box>
+                        <Typography
+                            variant="h3"
+                            fontWeight="500"
+                        >
+                            {props.title}
+                        </Typography>
+                    </Box>
 
-                {/* Location */}
-                <Box mb={6}>
-                    <Typography
-                        variant="body1"
-                        fontWeight="400"
-                        color={ThemeCoreSingleton.paletteManager.getColor("grey", undefined, "700")}
-                    >
-                        {`${props.shopName} · ${props.location}`}
-                    </Typography>
-                </Box>
+                    {/* Location */}
+                    <Box mb={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight="400"
+                            color={ThemeCoreSingleton.paletteManager.getColor("grey", undefined, "700")}
+                        >
+                            {`${props.shopName} · ${props.location}`}
+                        </Typography>
+                    </Box>
 
-                {/* Price */}
-                <Box alignX="left">
-                    <Box alignY="center">
-                        {/* Sale */}
-                        <React.Fragment>
-                        {
-                            (props.price.sale !== undefined) && (
-                                <Box mr={10}>
-                                    <Typography
-                                        variant="h4"
-                                        fontWeight="500"
-                                        color={ThemeCoreSingleton.paletteManager.getColor("primary", "main")}
-                                    >
-                                        {
-                                            `${100 - parseInt(String(props.price.sale / props.price.original * 100))}%`
-                                        }
-                                    </Typography>
-                                </Box>
-                            )
-                        }
-                        </React.Fragment>
-
-                        <Box mr={10}>
-                            <Typography variant="h4" fontWeight="500">
-                                {`${(props.price.sale !== undefined) ? props.price.sale : props.price.original}만원`}
-                            </Typography>
-                        </Box>
-
-                        <React.Fragment>
-                        {
-                            (props.price.sale !== undefined) && (
-                                <Box alignY="center">
-
-                                    <Box>
-                                        <Typography 
-                                            variant="body1" 
+                    {/* Price */}
+                    <Box alignX="left">
+                        <Box alignY="center">
+                            {/* Sale */}
+                            <React.Fragment>
+                            {
+                                (props.price.sale !== undefined) && (
+                                    <Box mr={10}>
+                                        <Typography
+                                            variant="h4"
                                             fontWeight="500"
-                                            color={ThemeCoreSingleton.paletteManager.getColor("grey", undefined, "700")}
-                                            decorationLine={"line-through"}
+                                            color={ThemeCoreSingleton.paletteManager.getColor("primary", "main")}
                                         >
-                                            {`${props.price.original}만원`}
+                                            {
+                                                `${100 - parseInt(String(props.price.sale / props.price.original * 100))}%`
+                                            }
                                         </Typography>
                                     </Box>
-                                </Box>
-                            )
-                        }
-                        </React.Fragment>
+                                )
+                            }
+                            </React.Fragment>
+
+                            <Box mr={10}>
+                                <Typography variant="h4" fontWeight="500">
+                                    {`${(props.price.sale !== undefined) ? props.price.sale : props.price.original}만원`}
+                                </Typography>
+                            </Box>
+
+                            <React.Fragment>
+                            {
+                                (props.price.sale !== undefined) && (
+                                    <Box alignY="center">
+
+                                        <Box>
+                                            <Typography 
+                                                variant="body1" 
+                                                fontWeight="500"
+                                                color={ThemeCoreSingleton.paletteManager.getColor("grey", undefined, "700")}
+                                                decorationLine={"line-through"}
+                                            >
+                                                {`${props.price.original}만원`}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                )
+                            }
+                            </React.Fragment>
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
-        </CardTypePost> 
+            </CardTypePost>
+        </Link>
     )
 }
 
