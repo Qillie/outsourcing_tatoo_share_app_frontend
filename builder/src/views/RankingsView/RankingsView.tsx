01267dacfab878089bb8ffdda34cbe89f32b7265
Qillie <aorkzlr@qillie.com>
2022-09-25 17:16:57 +0900

    Update RankingsView

--- builder/src/views/RankingsView/RankingsView.tsx
+++ builder/src/views/RankingsView/RankingsView.tsx
@@ -14,22 +14,104 @@ import { Thumbnail } from "../../core/display";
 import { ThemeCoreSingleton } from "../../core/design";
 import { ThumbnailTypePost } from "../../modules";
 import { IconButton } from "../../core/input";
+import CardProductPost from "../../widgets/CardProductPost";
 
 //* Import interfaces
 import IRankingsView from "./interfaces/IRankingsView"
 import VerticalThumbnailPost from "../../widgets/VerticalThumbnailPost";
+import IProduct from "../../widgets/CardProductPost/interfaces/IProduct";
 
 
 const RankingsView = (props: IRankingsView) => {
-    //* Modules
-
-    
+    //* States
+    const [products, setProducts] = React.useState<IProduct[]>([
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 16,
+                sale: 5
+            }
+        },
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 15
+            }
+        },
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 15,
+                sale: 12
+            }
+        },
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 15
+            }
+        },
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 15,
+                sale: 12
+            }
+        },
+        {
+            title: "가족 생일 타투",
+            shopName: "Black hands tatoo",
+            link: "/item",
+            location: "경기/수원",
+            src: "https://picsum.photos/200/300",
+            tags: ["당일예약", "주차가능"],
+            price: {
+                original: 15
+            }
+        }
+    ])
 
     return (
         <ScrollView>
             <Box pb={200} flexDirection="column">
                 <Box flexDirection="column">
-                
+                    {/* List */}
+                    <Box flexDirection="column" px={14}>
+                        {
+                            products.map((product) => (
+                                <Box mb={20}>
+                                    <CardProductPost
+                                        {...product}
+                                    />
+                                </Box>
+                            ))
+                        }
+                    </Box>
                 </Box>
             </Box>
         </ScrollView>
